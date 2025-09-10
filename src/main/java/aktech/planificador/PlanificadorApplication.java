c0d63d1df45c028615d3a92da64ea674e8aaea0d
seba <sebastianconde60@gmail.com>
2025-09-10 00:47:48 -0300

    fix env

--- src/main/java/aktech/planificador/PlanificadorApplication.java
+++ src/main/java/aktech/planificador/PlanificadorApplication.java
@@ -3,19 +3,11 @@ package aktech.planificador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PlanificadorApplication {
 
 	public static void main(String[] args) {
-		// Cargar variables del archivo .env al entorno
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		dotenv.entries().forEach(entry -> {
-			if (System.getenv(entry.getKey()) == null) {
-				System.setProperty(entry.getKey(), entry.getValue());
-			}
-		});
 		SpringApplication.run(PlanificadorApplication.class, args);
 	}
 
